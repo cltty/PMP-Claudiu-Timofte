0789eda067ba12aae10e740938300e85109ca280
Claudiu Timofte <59256981+cltty@users.noreply.github.com>
2021-01-05 11:59:25 +0200

    Update Ex1.scala

--- PMP-Template-master/src/main/scala/Lab11/Ex1.scala
+++ PMP-Template-master/src/main/scala/Lab11/Ex1.scala
@@ -2,14 +2,18 @@ package Lab11
 
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.algorithm.factored.VariableElimination
 
 object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
+	val citizenIsPresident = Flip(0.00000002)
+  	val citizenIsLeftHanded = Flip(0.1)
+  	val presidentIsLeftHanded = Flip(0.5)
+	
+	citizenIsLeftHanded.observe(true)
+  	
+	println("P(POTUS|LH): " + VariableElimination.probability(citizenIsPresident, true))
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
-	}
+	def main(args: Array[String]) {
+  	}
+
 }
